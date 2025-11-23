72157eeae283fd8c34b7719456771312cc04c375
Igu2012 <igormombergdemeira@gmail.com>
2025-11-23 13:19:49 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 device_index = 2 # make sure to keep changing the number until it prints "Stereo Mix" or your roblox audio output
 OUTPUT_FILENAME = 'desktop_audio.wav' # you dont need to change this one (make sure to keep the '.wav' on the end!)
 
-recordingTime = 3 # how much time it record (when you use the start prefix key)
+recordingTime = 3 # how much time it record (when you press the start prefix key)
 prefixKeyStart = 'ctrol_l' # the key it starts recording
 prefixKeyStop = 'esc' # the key it stops recording and kills the program
 
 # time between each letter typed
 min = 0.065
+
 max = 0.290
